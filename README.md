d9bcb778420be3febb2b6e413338769568b34d53
LeFnord <pscholz.le@gmail.com>
2023-06-26 22:54:17 +0200

    Uses newer stack to build the pack.
    - removes language traning data
    - try to fix the failure
    - adds train data files … not all, cause the resulting tarball have to be less them 100MB

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Heroku Buildpack Tesseract
 
-This package provides a custom Heroku buildpack providing the [Tesseract OCR](https://github.com/tesseract-ocr/tesseract) binary,
-for Heroku 20 and 22 stack.
-
-
 This is a buildpack adding a heroku-compatible version of the [tesseract](https://github.com/tesseract-ocr/tesseract)
-package. It uses the most recent 5.x version and supports stack 22. Actual all language trainee data are included.
+package. It uses the most recent 5.x version and supports stack 22.
 
-# General build instructions
+## General build instructions
 
-Ensure Docker is running and then un `make` on the console.
+Ensure Docker is running and then run `make` on the console.
 This will generate some tarballs you can check in.
+
+## Usage
+
+Add this URL: https://github.com/aredotna/heroku-buildpack-tesseract
--- build.sh
+++ build.sh
@@ -6,26 +6,35 @@ set -x
 VERSION=5.3.1
 STACK=heroku-22
 
+
 echo " - - - -> Install dependencies"
 export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
-apt-get install -y libleptonica-dev libpng-dev libtiff-dev libtool g++ pkg-config libgif-dev libicu-dev libpango1.0-dev cmake
+apt-get install -y \
+  autoconf automake autotools-dev libtool g++ pkg-config cmake \
+  libcurl4-openssl-dev libleptonica-dev libpng-dev libtiff-dev \
+  libgif-dev libicu-dev libpango1.0-dev
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 
-echo " - - - -> Fetching and Unpacking Tesseract-OCR sources"
+
+echo " - - - -> Create and go into build dir"
 mkdir -p /build
 cd /build || exit 1
+
+
+echo " - - - -> Fetching and Unpacking Tesseract sources"
 curl --fail -sq -LOJ "https://github.com/tesseract-ocr/tesseract/archive/refs/tags/${VERSION}.tar.gz"
 tar --strip-components=1 -zxvf "tesseract-${VERSION}.tar.gz"
 
+
+echo " - - - -> Build and install Tesseract from sources"
 mkdir build
 cd build || exit 1
-
-echo " - - - -> Build Tesseract-OCR from sources"
-CFLAGS="-mtune=generic" CXXFLAGS="-mtune=generic" cmake -DBUILD_SHARED_LIBS=OFF -DGRAPHICS_DISABLED=ON -DBUILD_TRAINING_TOOLS=ON -DMARCH_NATIVE_OPT=OFF ..
-
+# -DBUILD_SHARED_LIBS=OFF
+CFLAGS="-mtune=generic" CXXFLAGS="-mtune=generic" cmake -DGRAPHICS_DISABLED=ON -DBUILD_TRAINING_TOOLS=OFF -DMARCH_NATIVE_OPT=OFF ..
 make
+
 mkdir -p /src/tesseract-${VERSION}-${STACK}/bin
 mv bin/tesseract /src/tesseract-${VERSION}-${STACK}/bin/
 
@@ -33,6 +42,8 @@ mkdir -p /src/tesseract-${VERSION}-${STACK}/lib
 cp -P /lib/x86_64-linux-gnu/liblept*so* /src/tesseract-${VERSION}-${STACK}/lib/
 cp -P /lib/x86_64-linux-gnu/libgif*so* /src/tesseract-${VERSION}-${STACK}/lib/
 
+
+echo " - - - -> Install Tesseract train data"
 mkdir -p /src/tesseract-${VERSION}-${STACK}/share
 curl --fail -sq -Lo "/src/tesseract-${VERSION}-${STACK}/share/chi_sim.traineddata" "https://github.com/tesseract-ocr/tessdata/raw/main/chi_sim.traineddata"
 curl --fail -sq -Lo "/src/tesseract-${VERSION}-${STACK}/share/deu.traineddata" "https://github.com/tesseract-ocr/tessdata/raw/main/deu.traineddata"
@@ -51,3 +62,8 @@ cd /src
 umask 000
 tar -czpf tesseract-${VERSION}-${STACK}.tar.gz tesseract-${VERSION}-${STACK}
 # rm -rf tesseract-${VERSION}-${STACK}
+
+
+# Failures:
+# tesseract: error while loading shared libraries: libarchive.so.13
+# Error opening data file /app/vendor/tesseract/share/eng.traineddata
